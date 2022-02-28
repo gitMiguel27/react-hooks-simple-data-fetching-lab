3b1030b9fc9f9a12ed8fe7a356efbe6e728fb60c
Miguel Nazario <mnazario35@gmail.com>
2022-02-28 15:45:42 -0500

    commit tree 9992a2246f66bd02353bce8e66d2eaa569832199

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,25 @@
 // create your App component here
+import React, { useEffect, useState } from "react";
+
+function App() {
+    const [randomDog, setRandomDog] = useState([]);
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+            .then(resp => resp.json())
+            .then(dogData => {
+                // console.log(dogData.message)
+                setRandomDog(dogData.message);
+                setIsLoaded(true);
+            });
+    }, [])
+
+    if (!isLoaded) return <p>Loading...</p>;
+
+    return (
+        <img src={randomDog} alt='A Random Dog'/>
+    );
+}
+
+export default App;
